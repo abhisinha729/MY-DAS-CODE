05655f6db8e357ae0bf0bcf11159246b758ed9b8
Abhishek Kumar Sinha <abhisinha729@gmail.com>
2025-06-09 11:49:10 +0530

    Time: 7 ms (44.79%) | Memory: 18.9 MB (49.27%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,17 +3,24 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        lis=[]
-        count=0
-        for i in nums:
-            if i==0:
-                count+=1
-            else:
-                lis.append(i)
-        for i in range (count):
-            lis.append(0)
-        for i in range(len(lis)):
-            nums[i]=lis[i]
-        return nums    
+        # lis=[]
+        # count=0
+        # for i in nums:
+        #     if i==0:
+        #         count+=1
+        #     else:
+        #         lis.append(i)
+        # for i in range (count):
+        #     lis.append(0)
+        # for i in range(len(lis)):
+        #     nums[i]=lis[i]
+        # return nums    
+        i,j=0,0
+        while(j<len(nums)):
+             if (nums[j]!=0):
+                nums[i],nums[j]=nums[j],nums[i]
+                i+=1
+             j+=1 
+        return nums       
 
 
