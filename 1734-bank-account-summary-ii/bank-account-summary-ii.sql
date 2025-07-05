0bc61fa1d66b9d575501ea8f7a6290e185b0503e
Abhishek Kumar Sinha <abhisinha729@gmail.com>
2025-07-05 15:49:08 +0530

    Time: 882 ms (27.53%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1734-bank-account-summary-ii/bank-account-summary-ii.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select u.name as name,sum(t.amount)as balance 
+from users u left join transactions t
+on u.account=t.account
+group by t.account
+having balance>"10000"
