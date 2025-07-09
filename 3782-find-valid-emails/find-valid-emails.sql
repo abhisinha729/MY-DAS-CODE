f70856c0472097f708e05058c059bfe734888744
Abhishek Kumar Sinha <abhisinha729@gmail.com>
2025-07-09 23:33:47 +0530

    Time: 371 ms (18.37%) | Memory: 0B (100.00%) - LeetSync

--- 3782-find-valid-emails/find-valid-emails.sql
+++ 3782-find-valid-emails/find-valid-emails.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement belo
 select user_id,email
 from users
-where email regexp '^[a-zA-Z0-9_]*@[a-zA-Z]*\\.com'
+where email regexp '^[a-zA-Z0-9]*@[a-zA-Z]*\\.com'
 order by user_id
