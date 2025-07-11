ff6c2514deeb08215527c60d7de91dcadfae218d
Abhishek Kumar Sinha <abhisinha729@gmail.com>
2025-07-11 18:41:54 +0530

    Time: 484 ms (23.99%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -5,6 +5,6 @@ set N=N-1;
       select distinct (salary) 
       from employee 
       order by salary desc
-       limit N,1 # limit tell how many row to return and offset tell how many row to skip
-  );
+       limit 1 offset N # limit tell how many row to return and offset tell how many row to return 
+  );                     # here we can also write limit N,1 (shorthand )
 end
