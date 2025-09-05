5c7fbaf5600f6af644967175cf236d05d74c03d5
Abhishek Kumar Sinha <abhisinha729@gmail.com>
2025-09-05 19:39:33 +0530

    Time: 1268 ms (6.71%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,4 +1,12 @@
 # Write your MySQL query statement below
 
 -- delete from person where id not in (select id from (select min(id) as id from person group by email) as temp);
-delete p1 from person p1,person p2 where p1.email=p2.email and p1.id>p2.id; #by using self join
+-- delete p1 from person p1,person p2 where p1.email=p2.email and p1.id>p2.id; #by using self join.
+
+delete p1
+from person p1  join person p2
+on p1.email=p2.email
+where p1.id>p2.id and p1.email=p2.email
+
+
+
