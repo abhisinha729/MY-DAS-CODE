2aaad5c1a0237218b1bc52615e66b58816a81337
Abhishek Kumar Sinha <abhisinha729@gmail.com>
2025-09-05 19:59:53 +0530

    Time: 502 ms (44.48%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,6 +1,12 @@
 # Write your MySQL query statement below
+-- select w1.id
+-- from weather w1
+-- inner join weather w2
+-- where datediff(w1.recorddate,w2.recorddate)=1# datediff  is function that give difference between dates
+-- and w1.temperature > w2.temperature
+
+
 select w1.id
-from weather w1
-inner join weather w2
-where datediff(w1.recorddate,w2.recorddate)=1# datediff  is function that give difference between dates
-and w1.temperature > w2.temperature
+from weather w1 join weather w2
+-- on w1.id=w2.id
+where datediff(w1.recorddate,w2.recorddate)=1 and w1.temperature > w2.temperature
